2abb8167518b8212b44c336d24e13e2b4f0b72ec
Jorge Vicens Valor <gromit.jvv@gmail.com>
2020-07-29 20:22:31 +0200

    🚧 226. Navigating to the Contact Data Component

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
+import axios from '../../axios-order';
+
 import Aux from '../../hoc/Aux/Aux';
-import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
+import Burger from '../../components/Burger/Burger';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 import Spinner from '../../components/UI/Spinner/Spinner';
 import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
-import axios from '../../axios-order';
 
 
 const INGREDIENT_PRICES = {
--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
 
+import ContactData from './ContactData/ContactData';
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
-
 
 class Checkout extends Component {
     state = {
@@ -28,7 +29,7 @@ class Checkout extends Component {
     }
 
     checkoutContinuedHandler = () => {
-        this.props.history.replace('/checkout/contact-data');
+        this.props.history.replace( '/checkout/contact-data' );
     }
 
     render() {
@@ -38,6 +39,11 @@ class Checkout extends Component {
                     ingredients={this.state.ingredients}
                     checkoutCancelled={this.checkoutCancelledHandler}
                     checkoutContinued={this.checkoutContinuedHandler}
+                />
+
+                <Route
+                    path={this.props.match.path + '/contact-data'}
+                    component={ContactData}
                 />
             </div>
         );
