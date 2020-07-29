3e7e3a28d1e34ea647c186bf172ff5ba47376cf1
Jorge Vicens Valor <gromit.jvv@gmail.com>
2020-07-29 22:01:31 +0200

    🚧 227. Order Submission & Passing Data Between Pages

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -76,33 +76,11 @@ class BurgerBuilder extends Component {
 
     purchaseContinueHandler = () => {
         // alert('you continue');
-        // this.setState({ loading: true })
-        // const order = {
-        //     ingredients: this.state.ingredients,
-        //     price: this.state.totalPrice,
-        //     customer: {
-        //         name: 'Jorge Vicens',
-        //         address: {
-        //             street: 'Blop street',
-        //             cp: '03202',
-        //             country: 'Spain'
-        //         },
-        //         email: 'uno@dos'
-        //     },
-        //     deliveryMethod: 'fastest'
-        // }
-
-        // axios.post('/orders.json', order)
-        //     .then(response => {
-        //         this.setState({ loading: false, purchasing: false })
-        //     })
-        //     .catch(error => {
-        //         this.setState({ loading: false, purchasing: false })
-        //     });
         const queryParams = [];
         for (let i in this.state.ingredients) {
             queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
         }
+        queryParams.push('price=' + this.state.totalPrice);
         const queryString = queryParams.join('&');
 
         this.props.history.push({
