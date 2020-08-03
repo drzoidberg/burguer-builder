ca2453428b125dbbc50775a748f0e6ca0f66a0e3
Jorge Vicens Valor <gromit.jvv@gmail.com>
2020-08-03 10:26:45 +0200

    🚧 276. Finishing the Reducer for Ingredients

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -95,13 +95,13 @@ class BurgerBuilder extends Component {
 
     componentDidMount() {
         console.log(this.props);
-        axios.get('https://react-burger-builder-5db37.firebaseio.com/ingredients.json')
-            .then(response => {
-                this.setState({ ingredients: response.data });
-            })
-            .catch(error => {
-                this.setState({ error: true })
-            })
+        // axios.get('https://react-burger-builder-5db37.firebaseio.com/ingredients.json')
+        //     .then(response => {
+        //         this.setState({ ingredients: response.data });
+        //     })
+        //     .catch(error => {
+        //         this.setState({ error: true })
+        //     })
     }
 
     render() {
