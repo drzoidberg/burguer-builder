af2fced066a030177c2647d76fff2b7494a9f2b0
Jorge Vicens Valor <gromit.jvv@gmail.com>
2020-08-03 22:03:03 +0200

    🚧 278. Working on the Total Price Calculation

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -16,7 +16,6 @@ import * as actionTypes from '../../store/actions';
 class BurgerBuilder extends Component {
     state = {
         currency: '€',
-        purchasable: false,
         purchasing: false,
         loading: false,
         error: false
@@ -30,7 +29,7 @@ class BurgerBuilder extends Component {
             .reduce((sum, el) => {
                 return sum + el;
             }, 0)
-        this.setState({ purchasable: sum > 0 });
+        return sum > 0;
     };
 
     purchaseHandler = () => {
@@ -91,7 +90,7 @@ class BurgerBuilder extends Component {
                         ingredientRemoved={this.props.onIngredientRemoved}
                         ordered={this.purchaseHandler}
                         price={this.props.price}
-                        purchasable={this.state.purchasable}
+                        purchasable={this.updatePurchaseState(this.props.ings)}
                     />
                 </Aux>
             );
