44ebef80e5fc2a17e5c3a7e572164e5e2c91235c
Jorge Vicens Valor <gromit.jvv@gmail.com>
2020-08-10 21:02:44 +0200

    🚧 328. Storing the Token

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -21,7 +21,7 @@ export const authFail = (error) => {
     }
 }
 
-export const auth = (email, password) => {
+export const auth = (email, password, isSignup) => {
     return dispatch => {
         dispatch(authStart());
         const authData = {
@@ -30,7 +30,11 @@ export const auth = (email, password) => {
             returnSecureToken: true
         }
 
-        axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCQHivH6hVZe20WKFs_n3rH0kEPxUW6WNA', authData)
+        let url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCQHivH6hVZe20WKFs_n3rH0kEPxUW6WNA';
+        if (!isSignup) {
+            url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCQHivH6hVZe20WKFs_n3rH0kEPxUW6WNA';
+        }
+        axios.post(url, authData)
             .then(response => {
                 console.log(response);
                 dispatch(authStart(response.data))
