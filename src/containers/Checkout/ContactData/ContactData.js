ac7f95d535cbab29cddc113f5fcbee18dab352a6
Jorge Vicens Valor <gromit.jvv@gmail.com>
2020-08-11 14:22:01 +0200

    🚧 340. Displaying User Specific Orders

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -105,7 +105,8 @@ class ContactData extends Component {
             ingredients: this.props.ings,
             price: this.props.price,
             deliveryMethod: 'fastest',
-            orderData: formData
+            orderData: formData,
+            userId: this.props.userId
         }
 
         this.props.onOrderBurger(order, this.props.token);
@@ -215,7 +216,8 @@ const mapStateToProps = state => {
         ings: state.burgerBuilder.ingredients,
         price: state.burgerBuilder.totalPrice,
         loading: state.order.loading,
-        token: state.auth.token
+        token: state.auth.token,
+        userId: state.auth.userId
     }
 }
 
