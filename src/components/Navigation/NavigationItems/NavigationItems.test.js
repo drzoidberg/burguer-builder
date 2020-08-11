5bdf1e2f5fba182981f1d5683decdca11747d2ca
Jorge Vicens Valor <gromit.jvv@gmail.com>
2020-08-11 18:37:42 +0200

    🚧 357. Testing Components Continued

--- src/components/Navigation/NavigationItems/NavigationItems.test.js
+++ src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -8,8 +8,17 @@ import NavigationItem from './NavigationItem/NavigationItem';
 configure({ adapter: new Adapter() });
 
 describe('<NavigationItems />', () => {
-    it('Should render two <NavigationItems /> elements if not aithenticated', () => {
-        const wrapper = shallow(<NavigationItems />);
+    let wrapper;
+    beforeEach(() => {
+        wrapper = shallow(<NavigationItems />);
+    });
+
+    it('Should render two <NavigationItems /> elements if not authenticated', () => {
         expect(wrapper.find(NavigationItem)).toHaveLength(2);
+    });
+    it('Should render thiree <NavigationItems /> elements if authenticated', () => {
+        // wrapper = shallow(<NavigationItems isAuthenticated/>);
+        wrapper.setProps({ isAuthenticated: true })
+        expect(wrapper.find(NavigationItem)).toHaveLength(3);
     });
 })
