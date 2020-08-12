e341c366e44fb8366cb45e9a61680214acee091c
Jorge Vicens Valor <gromit.jvv@gmail.com>
2020-08-12 21:56:58 +0200

    🚧 422. Diving Deeper into Sagas

--- src/store/actions/order.js
+++ src/store/actions/order.js
@@ -1,5 +1,5 @@
 import * as actionTypes from './actionTypes';
-import axios from '../../axios-order';
+// import axios from '../../axios-order';
 
 export const purchaseBurgerSuccess = (id, orderData) => {
     return {
