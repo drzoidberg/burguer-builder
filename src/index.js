39d0d6b492588eed5c2f5d13ddc4b541e76c441a
Jorge Vicens Valor <gromit.jvv@gmail.com>
2020-08-12 19:57:45 +0200

    🚧 415. Moving Logic from the Action Creator to a Saga

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ import createSagaMiddleware from 'redux-saga';
 import burgerBuilderReducer from './store/reducers/burgerBuilder';
 import orderReducer from './store/reducers/order';
 import authReducer from './store/reducers/auth';
-import { logoutSaga } from './store/sagas/auth';
+import { watchAuth } from './store/sagas';
 
 
 const composeEnhancers = process.env.NODE_ENV === 'development'
@@ -34,7 +34,7 @@ const store = createStore(
         applyMiddleware(thunk, sagaMiddleware)
 ));
 
-sagaMiddleware.run(logoutSaga);
+sagaMiddleware.run(watchAuth);
 
 const app = (
     <Provider store={store}>
