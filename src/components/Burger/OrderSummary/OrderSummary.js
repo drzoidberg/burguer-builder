4553c48d90e4640c6f3b266db9de2ebb3b6c0f92
Jorge Vicens Valor <gromit.jvv@gmail.com>
2020-08-14 16:01:04 +0200

    🚧 460. Using React.memo() & More!

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,37 +1,32 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Aux from '../../../hoc/Aux/Aux';
 import Button from '../../UI/Button/Button';
 
-class OrderSummary extends Component {
-    componentDidUpdate() {
-        // console.log('[OrderSummary] componentDidUpdate');
-    }
+const OrderSummary = (props) => {
 
-    render() {
-        const ingredientSummary = Object.keys(this.props.ingredients)
+    const ingredientSummary = Object.keys(props.ingredients)
         .map(ingredientKey => {
             return (
                 <li key={ingredientKey}>
-                    <span style={{textTransform: 'capitalize'}}>{ingredientKey}:</span> {this.props.ingredients[ingredientKey]}
+                    <span style={{ textTransform: 'capitalize' }}>{ingredientKey}:</span> {props.ingredients[ingredientKey]}
                 </li>
             );
         });
 
-        return(
-            <Aux>
-                <h3>Your Order</h3>
-                <p>A delicious burger with the following ingredients:</p>
-                <ul>
-                    {ingredientSummary}
-                </ul>
-                <p>Continue to Checkout?</p>
-                <p><strong>Total Price: {this.props.price.toFixed(2)}{this.props.currency}</strong></p>
-                <Button btnType="Danger" clicked={this.props.purchaseCancelled}>CANCEL</Button>
-                <Button btnType="Success" clicked={this.props.purchaseContinued}>CONTINUE</Button>
+    return (
+        <Aux>
+            <h3>Your Order</h3>
+            <p>A delicious burger with the following ingredients:</p>
+            <ul>
+                {ingredientSummary}
+            </ul>
+            <p>Continue to Checkout?</p>
+            <p><strong>Total Price: {props.price.toFixed(2)}{props.currency}</strong></p>
+            <Button btnType="Danger" clicked={props.purchaseCancelled}>CANCEL</Button>
+            <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
 
-            </Aux>
-        );
-    }
+        </Aux>
+    );
 }
 
 
